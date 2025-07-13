1ccc85c04aedfe8927db6f8189223a4191116183
pascal369 <138413758+pascal369@users.noreply.github.com>
2025-07-13 10:22:50 +0900

    Add files via upload

--- CompressionCoilSpring.py
+++ CompressionCoilSpring.py
@@ -36,37 +36,47 @@ class Ui_Dialog(object):
         self.lineEdit_dia.setObjectName("Wire_dia")
         
         #ピッチ Pitch
-        self.label_Pitch = QtGui.QLabel('Pitch',Dialog)
-        self.label_Pitch.setGeometry(QtCore.QRect(10, 38, 150, 12))
-        self.lineEdit_Pitch = QtGui.QLineEdit('2.0',Dialog)
-        self.lineEdit_Pitch.setGeometry(QtCore.QRect(80, 35, 50, 20))
-        self.lineEdit_Pitch.setAlignment(QtCore.Qt.AlignCenter)
-        self.lineEdit_Pitch.setObjectName("Pitch")
+        #self.label_Pitch = QtGui.QLabel('Pitch',Dialog)
+        #self.label_Pitch.setGeometry(QtCore.QRect(10, 38, 150, 12))
+        #self.lineEdit_Pitch = QtGui.QLineEdit('2.0',Dialog)
+        #self.lineEdit_Pitch.setGeometry(QtCore.QRect(80, 35, 50, 20))
+        #self.lineEdit_Pitch.setAlignment(QtCore.Qt.AlignCenter)
+        #self.lineEdit_Pitch.setObjectName("Pitch")
         #コイル径 Coildia
         self.label_coilDia = QtGui.QLabel('Coil_dia',Dialog)
-        self.label_coilDia.setGeometry(QtCore.QRect(10, 63, 150, 12))
+        self.label_coilDia.setGeometry(QtCore.QRect(10, 35, 150, 12))
         self.lineEdit_coilDia = QtGui.QLineEdit('10.0',Dialog)
-        self.lineEdit_coilDia.setGeometry(QtCore.QRect(80, 65, 50, 20))
+        self.lineEdit_coilDia.setGeometry(QtCore.QRect(80, 35, 50, 20))
         self.lineEdit_coilDia.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit_coilDia.setObjectName("CoilRa")
         #巻き数　turns
         self.label_Turns = QtGui.QLabel('Turns',Dialog)
-        self.label_Turns.setGeometry(QtCore.QRect(10, 88, 150, 12))
+        self.label_Turns.setGeometry(QtCore.QRect(10, 60, 150, 12))
         self.lineEdit_Turns = QtGui.QLineEdit('10.0',Dialog)
-        self.lineEdit_Turns.setGeometry(QtCore.QRect(80, 85, 50, 20))
+        self.lineEdit_Turns.setGeometry(QtCore.QRect(80, 60, 50, 20))
         self.lineEdit_Turns.setAlignment(QtCore.Qt.AlignCenter)
         self.lineEdit_Turns.setObjectName("CoilHight")
-
+        #ピッチ
+        self.label_spin=QtGui.QLabel('Pitch',Dialog)
+        self.label_spin.setGeometry(QtCore.QRect(10, 105, 150, 22))
+        self.spinBox=QtGui.QSpinBox(Dialog)
+        self.spinBox.setGeometry(75, 105, 100, 50)
+        self.spinBox.setMinimum(0.0)  # 最小値を0.0に設定
+        self.spinBox.setMaximum(360.0)  # 最大値を100.0に設定
+        self.spinBox.setValue(0.0)
+        self.spinBox.setAlignment(QtCore.Qt.AlignCenter)
         #作成
         self.pushButton = QtGui.QPushButton('Create',Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(10, 125, 60, 22))
+        self.pushButton.setGeometry(QtCore.QRect(10, 165, 60, 22))
         #更新
         self.pushButton2 = QtGui.QPushButton('upDate',Dialog)
-        self.pushButton2.setGeometry(QtCore.QRect(190, 125, 60, 22))
+        self.pushButton2.setGeometry(QtCore.QRect(190, 165, 60, 22))
         #インポート
         self.pushButton3 = QtGui.QPushButton('Import',Dialog)
-        self.pushButton3.setGeometry(QtCore.QRect(100, 125, 60, 22))
+        self.pushButton3.setGeometry(QtCore.QRect(100, 165, 60, 22))
         self.pushButton3.setObjectName("pushButton3")
+
+        self.spinBox.valueChanged[int].connect(self.spinMove) 
        
         QtCore.QObject.connect(self.pushButton2, QtCore.SIGNAL("pressed()"), self.update)
         self.retranslateUi(Dialog)
@@ -77,6 +87,12 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "compression coil spring", None))
         
+    def spinMove(self):
+        r1 = self.spinBox.value()
+        if r1==spreadsheet.getContents('Pitch'):
+            return
+        spreadsheet.set('Pitch',str(r1))
+        App.ActiveDocument.recompute() 
     def onImport(self):
         global spreadsheet
         selection = Gui.Selection.getSelection()
@@ -93,21 +109,20 @@ class Ui_Dialog(object):
                          spreadsheet = obj
 
                          self.lineEdit_dia.setText(spreadsheet.getContents('Wire_dia'))  
-                         self.lineEdit_Pitch.setText(spreadsheet.getContents('Pitch'))  
+                         #self.lineEdit_Pitch.setText(spreadsheet.getContents('Pitch'))  
                          self.lineEdit_coilDia.setText(spreadsheet.getContents('Coil_dia'))  
                          self.lineEdit_Turns.setText(spreadsheet.getContents('Turns')) 
-                         #print('bbbbbbbbbbbbbbbb')
+                         Pitch=spreadsheet.getContents('Pitch')
+                         self.spinBox.setValue(float(Pitch))
     def update(self):
          
          dia=self.lineEdit_dia.text()
-         Pitch=self.lineEdit_Pitch.text()
+         #Pitch=self.lineEdit_Pitch.text()
          Coil_dia=self.lineEdit_coilDia.text()
          Turns=self.lineEdit_Turns.text()
-         
          try:
-            
             spreadsheet.set('B2',dia)
-            spreadsheet.set('B3',Pitch)
+            #spreadsheet.set('B3',Pitch)
             spreadsheet.set('B4',Coil_dia)
             spreadsheet.set('B5',Turns)
             App.ActiveDocument.recompute()
