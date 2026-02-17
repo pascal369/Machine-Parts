50db313dcc89e24c8f305e064fa23ddbb05b49bc
pascal369 <138413758+pascal369@users.noreply.github.com>
2026-02-17 21:13:11 +0900

    Add files via upload

--- planetaryGears.py
+++ planetaryGears.py
@@ -401,16 +401,53 @@ class Ui_Dialog(object):
         self.label_N0.setText('(Zs+Zi)/n=(' + str(Zs) + '+' + str(Zi) + ')/' + str(n) + '=' + str(n0) + '  must integer')
         self.label_N02.setText('Zi-(Zs+2*Zp)=' + str(Zi) + '-' + '(' + str(Zs) + '+' + '2*' + str(Zp)+')' + '=' + str(n02)+'  must 0')
 
-    def create(self): 
+    def create(self):
+         doc=App.ActiveDocument
          fname='planetaryGears.FCStd'
          base=os.path.dirname(os.path.abspath(__file__))
          joined_path = os.path.join(base, 'prt_data','Gear_data',fname) 
-         try:
-            Gui.ActiveDocument.mergeProject(joined_path)
-         except:
-            doc=App.newDocument()
-            Gui.ActiveDocument.mergeProject(joined_path)
-         Gui.SendMsgToActiveView("ViewFit")   
+          # --- インポート前のオブジェクトリストを取得 ---
+         old_obj_names = [o.Name for o in doc.Objects]
+         
+         # マージ実行
+         Gui.ActiveDocument.mergeProject(joined_path)
+         doc.recompute() # 一旦再計算して内部IDを確定させる
+         # --- インポート後に増えたオブジェクトを特定 ---
+         new_objs = [o for o in doc.Objects if o.Name not in old_obj_names]
+         
+         if not new_objs:
+             print("Error: オブジェクトが読み込まれませんでした。")
+             return
+         #latticeBeamというラベルを持つものを優先的に探す
+         move_target = None
+         for o in new_objs:
+             if "planetaryAssy"  in o.Label or "planetaryAssy"  in o.Name:
+                 move_target = o
+                 break
+               
+
+         # 見つからなければ、新しく入ってきた最初のオブジェクトをターゲットにする
+         if not move_target:
+             move_target = new_objs[0]
+         view = Gui.ActiveDocument.ActiveView
+         callbacks = {}
+         def move_cb(info):
+             pos = info["Position"]
+             # 重要：ビュー平面上の3D座標を取得
+             p = view.getPoint(pos)
+             if move_target:
+                 move_target.Placement.Base = p
+                 #view.softRedraw()
+         def click_cb(info):
+             if info["State"] == "DOWN" and info["Button"] == "BUTTON1":
+                 # コールバック解除
+                 view.removeEventCallback("SoLocation2Event", callbacks["move"])
+                 view.removeEventCallback("SoMouseButtonEvent", callbacks["click"])
+                 App.ActiveDocument.recompute()
+                 print("Placed: " + move_target.Label)
+         # イベント登録
+         callbacks["move"] = view.addEventCallback("SoLocation2Event", move_cb)
+         callbacks["click"] = view.addEventCallback("SoMouseButtonEvent", click_cb)  
 
 class main():
         d = QtGui.QWidget()
