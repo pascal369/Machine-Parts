7e95e213b2ccf9acca6cc5c3732c58f4d8715e3c
pascal369 <138413758+pascal369@users.noreply.github.com>
2026-02-23 17:29:22 +0900

    Add files via upload

--- prt_data/RollBrg_data/ParamAngulaBallBrg.py
+++ prt_data/RollBrg_data/ParamAngulaBallBrg.py
@@ -35,28 +35,28 @@ class BallBrg:
         r=sa[3]
         n=sa[4]
         g0=sa[6]
-        t=(D-d)/6
+        t=(D-d)/7
         #VALUES#
         R1=d/2 #(radius of shaft/inner radius of inner ring)
         R2=R1+t #(outer radius of inner ring)
         
         R3=R1+2*t #(inner radius of outer ring)
+        
         R4=D/2 #(outer radius of outer ring)
         TH=B #(thickness of bearing)
         NBall=n #(number of balls)
         RBall=t #(radius of ball)
         RR=r  #(rounding radius for fillets)    
         CBall=((R3-R2)/2)+R2  #first coordinate of center of ball
+        
         PBall=TH/2 #second coordinate of center of ball
         #Inner Ring#
         B1=Part.makeCylinder(R1,TH)
-        #Part.show(IR2)
         B2=Part.makeCylinder(R2,TH)
         IR=B2.cut(B1)
         B11=Part.makeCylinder(R2,TH/2)
         B12=Part.makeCylinder(R2-0.4*t,TH/2)
         IR11=B11.cut(B12)
-        #Part.show(IR11)
         IR=IR.cut(IR11)
         #get edges and apply fillets
         Bedges=IR.Edges
@@ -67,15 +67,11 @@ class BallBrg:
         #create groove and show shape
         T1=Part.makeTorus(CBall,RBall)
         T1.translate(Base.Vector(0,0,TH/2))
-        #Part.show(T1)
         try:
             InnerRing=IRF.cut(T1)
-            #Part.show(InnerRing)
             c00=InnerRing
         except:
             return
-
-        #
         #Outer Ring#
         B3=Part.makeCylinder(R3,TH)
         B4=Part.makeCylinder(R4,TH)
@@ -92,12 +88,9 @@ class BallBrg:
              T2=Part.makeTorus(CBall,RBall)
              T2.translate(Base.Vector(0,0,TH/2))
              OuterRing=ORF.cut(T2)
-             #Part.show(OuterRing)
              c00=c00.fuse(OuterRing)
         except:
              return
-        #Part.show(c00)
-        #
         #Balls#
         for i in range(NBall):
             #Ball=Part.makeSphere(RBall)
@@ -123,7 +116,6 @@ class BallBrg:
             obj.mass=g0
             obj.ViewObject.Proxy=0
             pass  
-        #print(g0)  
         obj.Shape=c00    
             
         
