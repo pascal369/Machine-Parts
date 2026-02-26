809cda41948f8668b696a430b444dbfd61921040
pascal369 <138413758+pascal369@users.noreply.github.com>
2026-02-26 17:07:22 +0900

    Add files via upload

--- OneTouchWindow.py
+++ OneTouchWindow.py
@@ -109,8 +109,8 @@ class Ui_Dialog(object):
                          App.ActiveDocument.recompute()
 
     def create(self): 
-         
-         global fname
+         doc=App.ActiveDocument
+         #global fname
          key=self.comboBox_dia.currentText()
          if key=='130x200' or key=='200x300':
              fname='OneTouchWindow(1).FCStd'
@@ -120,12 +120,46 @@ class Ui_Dialog(object):
          base=os.path.dirname(os.path.abspath(__file__))
          joined_path = os.path.join(base, 'prt_data','OneTouchWindow_data',fname) 
          
-         try:
-            doc=App.activeDocument()
-            Gui.ActiveDocument.mergeProject(joined_path)
-         except:
-            doc=App.newDocument()
-            Gui.ActiveDocument.mergeProject(joined_path)
+         # --- インポート前のオブジェクトリストを取得 ---
+         old_obj_names = [o.Name for o in doc.Objects]
+          # マージ実行
+         Gui.ActiveDocument.mergeProject(joined_path)
+         doc.recompute() # 一旦再計算して内部IDを確定させる
+         # --- インポート後に増えたオブジェクトを特定 ---
+         new_objs = [o for o in doc.Objects if o.Name not in old_obj_names]
+         
+         if not new_objs:
+             print("Error: オブジェクトが読み込まれませんでした。")
+             return
+         #
+         move_target = None
+         for o in new_objs:
+             if "OneTouchWindow"  in o.Label or "OneTouchWindow"  in o.Name:
+                 move_target = o
+                 break
+
+         # 見つからなければ、新しく入ってきた最初のオブジェクトをターゲットにする
+         if not move_target:
+             move_target = new_objs[0]
+         view = Gui.ActiveDocument.ActiveView
+         callbacks = {}
+         def move_cb(info):
+             pos = info["Position"]
+             # 重要：ビュー平面上の3D座標を取得
+             p = view.getPoint(pos)
+             if move_target:
+                 move_target.Placement.Base = p
+                 #view.softRedraw()
+         def click_cb(info):
+             if info["State"] == "DOWN" and info["Button"] == "BUTTON1":
+                 # コールバック解除
+                 view.removeEventCallback("SoLocation2Event", callbacks["move"])
+                 view.removeEventCallback("SoMouseButtonEvent", callbacks["click"])
+                 App.ActiveDocument.recompute()
+                 print("Placed: " + move_target.Label)
+         # イベント登録
+         callbacks["move"] = view.addEventCallback("SoLocation2Event", move_cb)
+         callbacks["click"] = view.addEventCallback("SoMouseButtonEvent", click_cb)
         
 
          
